06eff55181b24553caca19314e118507abb77287
sppitts47794 <sppdevelopment47794@gmail.com>
2017-07-09 12:56:23 -0400

    Added CRUD

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,41 @@
+class NotesController < ApplicationController
+    before_action :find_note, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @notes = Note.all.order("created_at DESC")
+    end
+
+    def new
+        @note = Note.new
+    end
+
+    def create
+        @note = Note.new(note_params)
+
+        if @note.save
+            redirect_to @note
+        else
+            render 'new'
+        end
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def destroy
+
+    end
+
+    def find_note
+        @note = Note.find(params[:id])
+    end
+
+    def note_params
+        params.require(:note).permit(:title, :content)
+    end
+end

9d13129df1222a3ccc3e9e988eb03e248638458f
sppitts47794 <sppdevelopment47794@gmail.com>
2017-07-09 21:59:01 -0400

    Added Styles to views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'welcome/index'
+  resources :notes
+
+  authenticated :user do
+      root 'notes#index'
+  end
+
   root 'welcome#index'
 
-  resources :notes
+
 
 end
